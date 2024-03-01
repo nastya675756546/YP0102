bbdd5e816e49d8e516538c78eee2a91eebfcbc5f
nastya675756546 <128123902+nastya675756546@users.noreply.github.com>
2024-03-01 16:39:18 +0300

    Practice3

--- app/src/main/java/ru/btpit/nmedia/MainActivity.kt
+++ app/src/main/java/ru/btpit/nmedia/MainActivity.kt
@@ -1,47 +1,12 @@
 package ru.btpit.nmedia
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.btpit.nmedia.ui.theme.YpTheme
+import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            YpTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting( modifier: Modifier = Modifier) {
-    Text(
-        text = "Мы учимся в БТПИТ!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YpTheme {
-        Greeting()
     }
 }
